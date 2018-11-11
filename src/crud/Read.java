09cc43aac3f64025b8c06dd15b80cc143b5dc9aa
Janusz Sawicki <janusz_sawicki97@o2.pl>
2018-11-11 13:49:48 +0100

    Dodana operacja READ po ID

---
+++ src/crud/Read.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud;
+
+import entity.Rower;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author User
+ */
+public class Read {
+    public static void main(String [] args) {
+        
+        SessionFactory factory = new Configuration()
+                                .configure("hibernate.cfg.xml")
+                                .addAnnotatedClass(Rower.class)
+                                .buildSessionFactory();
+        
+        Session session = factory.getCurrentSession();
+        
+        try {
+            session.beginTransaction();
+            int id = 1;
+            
+            System.out.println("Retriving bike with id: " + id);
+            Rower retrivedBike = (Rower) session.get(Rower.class, id);
+            session.getTransaction().commit();
+            System.out.println("Retrived bike: " + retrivedBike);
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
